08d49676267a3533d48cb307a32ae1fa8da76b2d
Nathan Sawyer <sawyenat000@edmonds15.org>
2020-07-21 20:39:32 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+num = int(input("type in number"))
+if num > 10: 
+  print("number is greater than 10")
+elif num < 10:
+  print("number is less than 10")
+elif num == 10:
+  print("number is equal to 10")
